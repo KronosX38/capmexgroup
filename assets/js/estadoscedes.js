e69b04fadbab5e18a785cf65acf942c4d1a6d437
KronosX38 <j.cabanas8609@gmail.com>
2025-08-08 13:47:09 -0600

    Add files via upload

--- assets/js/estadoscedes.js
+++ assets/js/estadoscedes.js
@@ -1,26 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const estadoscedes = document.querySelectorAll('[data-bs-toggle="popover"]');
 
-  document.addEventListener('DOMContentLoaded', function () {
-    const estadoscedes = document.querySelectorAll('[data-bs-toggle="popover"]');
-    estadoscedes.forEach(el => {
-      const contentId = el.getAttribute('data-popover-content');
-      const contentElement = document.querySelector(contentId);
-      if (contentElement) {
-        new bootstrap.Popover(el, {
-          html: true,
-          content: contentElement.innerHTML,
-          customClass: 'mipopover',
-          sanitize: false
-        });
-      }
-    });
+  estadoscedes.forEach(el => {
+    const contentId = el.getAttribute('data-popover-content');
+    const contentElement = document.querySelector(contentId);
 
-    // Botón cerrar
-    document.body.addEventListener('click', function (e) {
-      if (e.target.classList.contains('cerrar-popover')) {
-        document.querySelectorAll('[data-bs-toggle="popover"]').forEach(el => {
-          const pop = bootstrap.Popover.getInstance(el);
-          if (pop) pop.hide();
-        });
-      }
-    });
+    if (contentElement) {
+      new bootstrap.Popover(el, {
+        html: true,
+        trigger: 'click',
+        placement: 'auto',
+        content: contentElement.innerHTML,
+        customClass: 'mipopover',
+        sanitize: false
+      });
+    }
   });
+
+  // Cerrar todos los popovers al hacer clic en un botón dentro de ellos
+  document.body.addEventListener('click', function (e) {
+    if (e.target.classList.contains('cerrar-popover')) {
+      document.querySelectorAll('[data-bs-toggle="popover"]').forEach(el => {
+        const pop = bootstrap.Popover.getInstance(el);
+        if (pop) pop.hide();
+      });
+    }
+  });
+});
